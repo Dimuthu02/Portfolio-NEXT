d3a711240458d62f441f6a2cabc7d6775eacd646
Dimuthu <Sanjuladimuthu2@gmail.com>
2025-12-08 21:39:33 -0800

    Fix Navbar component structure; update div wrapping and correct image tag usage

--- portfolio/components/Navbar.jsx
+++ portfolio/components/Navbar.jsx
@@ -5,8 +5,8 @@ import { assets } from "@/assets/assets";
 const Navbar = () => {
   return (
     <>
-    <div>
-      <image
+    <div className="fixed top-0 right-0 w-11/12 translate-y-[-80%]">
+   <Image src={assets.header_bg_color} alt="" className="w-full"></Image>
     </div>
 
 
