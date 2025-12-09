659187df9b627548a8fda58e514e7d37b20ab6f5
Dimuthu <Sanjuladimuthu2@gmail.com>
2025-12-08 21:22:57 -0800

    Refactor portfolio layout and styles; update fonts, remove unused assets, and enhance navbar functionality

---
+++ portfolio/components/Navbar.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import Image from "next/image";
+import { assets } from "@/assets/assets";
+
+const Navbar = () => {
+  return (
+    <>
+      <nav className="w-full fixed px-5 py-4 lg:px-8 xl:px-[8%] flex items-center justify-between z-50">
+        <a href="#top">
+          <Image
+            src={assets.logo}
+            width={120}
+            height={40}
+            className="w-28 cursor-pointer mr-14"
+            alt="Logo"
+          />
+        </a>
+
+        <ul className="hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3">
+          <li>
+            <a href="#top">Home</a>
+          </li>
+          <li>
+            <a href="#About">About me</a>
+          </li>
+          <li>
+            <a href="#Services">Services</a>
+          </li>
+          <li>
+            <a href="#Work">My Work</a>
+          </li>
+          <li>
+            <a href="#Contact">Contact me</a>
+          </li>
+        </ul>
+
+        <div>
+          <a className="hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4" href="#Contact">
+            Contact
+            <Image
+              src={assets.arrow_icon}
+              alt="Arrow"
+              width={12}
+              height={12}
+              className="w-3"
+            />
+          </a>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
